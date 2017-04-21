bec39cb3b7db9f5b05d7322facf2fe818ad5f374
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2017-04-21 08:00:24 +0200

    DigitSum etc

---
+++ src/FsLabApp/Lab.fs
@@ -0,0 +1,11 @@
+﻿module Lab
+
+let IsNumeric str = 
+    fst (System.Int32.TryParse(str))
+
+let Digits v = 
+    v |> string |> (fun e -> e.ToCharArray()) |> Array.map string |> Array.where IsNumeric |> Array.map int |> Array.toList 
+
+let DigitSum v =
+    v |> string |> Digits |> List.sum
+
---
+++ src/FsLabApp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    let res = "1123" |> Lab.DigitSum
+    printfn "%A" res
+    let s = System.Console.ReadKey()
+    0 // return an integer exit code
